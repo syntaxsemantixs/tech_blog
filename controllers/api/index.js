7602d46435fc168efe63c00d70d22dcc478df919
syntaxsemantixs <gormeno2030@gmail.com>
2023-03-13 15:31:28 -0400

    made adjustments to routes, js, and handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./post-routes');
 const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS tech_blog_db;
+CREATE DATABASE tech_blog_db;
